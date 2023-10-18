a690ba97f414fcecabcb985e8a7c590c3812df7b
HP <HP@LAPTOP-60G2658E>
2023-10-18 12:28:38 +0530

    Add project files.

---
+++ WebApplication6/Controllers/HomController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication6.Controllers
+{
+    public class HomController : Controller
+    {
+        // GET: Hom
+        public ActionResult Home()
+        {
+            return View();
+        }
+    }
+}
